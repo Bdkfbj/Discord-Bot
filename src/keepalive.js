daa5155efc9c01a8bc8c7d6b482f71fb569e4819
Bdkfbj <153366979+Bdkfbj@users.noreply.github.com>
2024-04-10 14:52:45 +0530

    Update keepalive.js

--- src/keepalive.js
+++ src/keepalive.js
@@ -1,16 +1,12 @@
-var agent = new KeepAliveAgent({ maxSockets: 1 });
+const http = require('http');
 
-var options = {
-  agent:agent,
-  headers: {"Connection":"Keep-Alive"}
-}
+const keepAliveServer = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Server is alive');
+});
 
-try {
-  var client = Soap.createClient(url);
+keepAliveServer.listen(3000, () => {
+  console.log('Keep alive server is running on port 3000');
+});
 
-  var result = client.myfirstfunction(args,options);
-
-//process result
-  result = client.mysecondfunction(args,options);
-
-}
+module.exports = keepAliveServer;
